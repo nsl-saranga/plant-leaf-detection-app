1aa7258d9fc729ce9960e0141ee09b04f0d5ac67
kaveen-lakmuthu <kaveensalakmuthu@gmail.com>
2024-03-09 22:11:01 +0530

    fixed little bug on opening gallery from drawer.

--- mobile_app/lib/home.dart
+++ mobile_app/lib/home.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
 
   Future<void> getImageFromGallery() async {
     final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
+        await ImagePicker().pickImage(source: ImageSource.camera);
 
     setState(() {
       _image = pickedFile != null ? File(pickedFile.path) : null;
